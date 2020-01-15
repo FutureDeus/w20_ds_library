b753964226e7b30047705ce3a3b8620a8cd4579e
FutureDeus <59908471+FutureDeus@users.noreply.github.com>
2020-01-14 23:24:06 -0800

    Update w20_ds_library.py

--- w20_ds_library.py
+++ w20_ds_library.py
@@ -4,3 +4,57 @@ dframe = TypeVar('pd.core.frame.DataFrame')
 
 def hello_ds():
     print("Big hello to you")
+
+def ordered_distances(target_vector:list, crowd_table:dframe, answer_column:str, dfunc:Callable) -> list:
+  assert isinstance(target_vector, list), f'target_vector not a list but instead a {type(target_vector)}'
+  assert isinstance(crowd_table, pd.core.frame.DataFrame), f'crowd_table not a dataframe but instead a {type(crowd_table)}'
+  assert isinstance(answer_column, str), f'answer_column not a string but instead a {type(answer_column)}'
+  assert callable(dfunc), f'dfunc not a function but instead a {type(dfunc)}'
+  assert answer_column in crowd_table, f'{answer_column} is not a legit column in crowd_table - check case and spelling'
+
+  #your code goes here
+  distance_list = [(index, euclidean_distance(target_vector, crowd_table.iloc[index].tolist()[1:])) for index,row in crowd_table.iterrows()]
+  distance_list.sort(key = sortSecond)
+  return distance_list
+
+def knn(target_vector:list, crowd_table:dframe, answer_column:str, k:int, dfunc:Callable) -> int:
+  assert isinstance(target_vector, list), f'target_vector not a list but instead a {type(target_vector)}'
+  assert isinstance(crowd_table, pd.core.frame.DataFrame), f'crowd_table not a dataframe but instead a {type(crowd_table)}'
+  assert isinstance(answer_column, str), f'answer_column not a string but instead a {type(answer_column)}'
+  assert answer_column in crowd_table, f'{answer_column} is not a legit column in crowd_table - check case and spelling'
+  assert crowd_table[answer_column].isin([0,1]).all(), f"answer_column must be binary"
+  assert callable(dfunc), f'dfunc not a function but instead a {type(dfunc)}'
+
+  #your code goes here
+  
+  top_k = [i for i,d in ordered_distances(target_vector, crowd_table, answer_column, dfunc)[:k]]
+  opinions = [crowd_table.loc[i, answer_column] for i in top_k]
+  winner = 1 if opinions.count(1) > opinions.count(0) else 0
+  return winner
+
+def knn_tester(test_table, crowd_table, answer_column, k, dfunc:Callable) -> dict:
+  assert isinstance(test_table, pd.core.frame.DataFrame), f'test_table not a dataframe but instead a {type(test_table)}'
+  assert isinstance(crowd_table, pd.core.frame.DataFrame), f'crowd_table not a dataframe but instead a {type(crowd_table)}'
+  assert isinstance(answer_column, str), f'answer_column not a string but instead a {type(answer_column)}'
+  assert answer_column in crowd_table, f'{answer_column} is not a legit column in crowd_table - check case and spelling'
+  assert crowd_table[answer_column].isin([0,1]).all(), f"answer_column must be binary"
+  assert callable(dfunc), f'dfunc not a function but instead a {type(dfunc)}'
+  
+  #your code here
+  collection = {(0, 0): 0, (0, 1): 0, (1, 0): 0, (1, 1): 0}
+  for index,row in test_table.iterrows():
+    node = (knn(test_table.iloc[index].tolist()[1:], crowd_table, answer_column, k, dfunc), test_table.loc[index, answer_column])
+    for i in collection:
+      if node == i:
+        collection[i] += 1
+  return collection
+
+def cm_accuracy(confusion_dictionary: dict) -> float:
+  assert isinstance(confusion_dictionary, dict), f'confusion_dictionary not a dictionary but instead a {type(confusion_dictionary)}'
+  
+  tp = confusion_dictionary[(1,1)]
+  fp = confusion_dictionary[(1,0)]
+  fn = confusion_dictionary[(0,1)]
+  tn = confusion_dictionary[(0,0)]
+  
+  return (tp+tn)/(tp+fp+fn+tn)
