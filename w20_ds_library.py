6f15ff09e136f2340ceeb8cec262bd9d93f95d9b
FutureDeus <59908471+FutureDeus@users.noreply.github.com>
2020-02-12 01:22:22 -0800

    Update w20_ds_library.py

--- w20_ds_library.py
+++ w20_ds_library.py
@@ -2,9 +2,9 @@ import pandas as pd
 import numpy as np
 import re
 import nltk
+from scipy import spatial
 from typing import TypeVar, Callable
 dframe = TypeVar('pd.core.frame.DataFrame')
-np.seterr(divide='ignore', invalid='ignore')
 
 char_set = '#!abcdefghijklmnopqrstuvwxyz'
 
@@ -80,7 +80,7 @@ def cosine_similarity(vect1:list ,vect2:list) -> float:
   assert len(vect1) == len(vect2), f"Mismatching length for vectors: {len(vect1)} and {len(vect2)}"
   
   #your code here
-  return np.nan_to_num(np.dot(vect1, vect2)/(np.linalg.norm(vect1) * np.linalg.norm(vect2)))
+  return 1 - spatial.distance.cosine(vect1, vect2)
 
 def inverse_cosine_similarity(vect1:list ,vect2:list) -> float:
   assert isinstance(vect1, list), f'vect1 is not a list but a {type(vect1)}'
